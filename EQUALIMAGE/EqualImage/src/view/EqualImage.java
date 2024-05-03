1159f08a96df27b78277b83deb1f268bb8f2fbdf
HermanoPSousa <hermanopereira.sousa@gmail.com>
2024-05-03 11:09:22 -0300

    Criando a classe EqualImage.java

---
+++ EQUALIMAGE/EqualImage/src/view/EqualImage.java
@@ -0,0 +1,81 @@
+
+package view;
+
+import java.awt.Dimension;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+
+/**
+ * @NAME....................: EqualImage.java
+ * @AUTHOR..................: Hermano Pereira de Sousa
+ * @COMPANY.................: Systems Development Center
+ * @DATE CREATE.............: 3 de mai. de 2024,10:56:44
+ * @DATE REFORMULATION......:
+ * @DATE END OF DEVELOPMENT.:
+ * @DATE MAINTENANCE........: 
+ * @OBJECTIVE...............: 
+ */
+public class EqualImage extends JFrame {
+    
+    /* start of variable declarations */
+    private JLabel lblImageFolderOrigin;
+    private JLabel lblImageFolderDestin;
+    
+    
+    /* end of variable declarations */
+
+
+
+    // <editor-fold defaultstate="collapsed" desc="CONSTRUTOR - EqualImage()">
+    /**
+     * @constructor: EqualImage()
+     * @objective: instantiate the class that was defined - EqualImage.java 
+     */
+    public EqualImage(){ 
+
+        /* start of variable declarations */
+        /* end of variable declarations */
+
+        /* begin assigning variables */
+        /* end of assignments to variables */
+
+        /* start of class instances */
+        /* end of class instances */
+
+        /* constructor body */
+            initComponent();
+        /* end of constructor body */
+
+    } // end of constructor EqualImage() </editor-fold>
+
+
+
+
+    // <editor-fold defaultstate="collapsed" desc="initComponent()">
+    /**
+     * 
+     * @method: initComponent()
+     * @objective:  - load class components, display them.
+     *              - change or receive changes to the property(s)
+     *                of the components.
+     *              - manipulate the action of window components.
+     */
+    private void initComponent(){
+
+        /* start of variable declarations */
+        /* end of variable declarations */
+
+        /* begin assigning variables */
+        /* end of assignments to variables */
+
+        /* start of class instances */
+        /* end of class instances */
+
+        /* method body */
+        setSize(new Dimension(882, 568));
+        setLocationRelativeTo(null);
+        /* end of method body */
+
+    } // end of method iniciarComponentes() </editor-fold>
+
+} // end of class EqualImage
